42696297e2678a40392febd4c1bf6ab4a9d8e581
SHU <44566893+SHU95@users.noreply.github.com>
2021-02-25 14:31:30 +0900

    commit
    同じ階層にDBとのやりとりをinterfaceで定義して、呼び出す。

---
+++ interfaces/database/sql_handler.go
@@ -0,0 +1,9 @@
+package database
+
+import "github.com/jinzhu/gorm"
+
+type SqlHandler interface {
+	Find(interface{}, ...interface{}) *gorm.DB
+	Create(interface{}) *gorm.DB
+	Save(interface{}) *gorm.DB
+}
