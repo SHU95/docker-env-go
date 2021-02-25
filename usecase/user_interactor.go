6a989a9fb90d0a9de234ae88de087d76426ec991
SHU <44566893+SHU95@users.noreply.github.com>
2021-02-25 14:34:11 +0900

    commit
    databaseからcontrollerへ情報を渡す
    appに近い感覚

---
+++ usecase/user_interactor.go
@@ -0,0 +1,24 @@
+package usecase
+
+import (
+	"github.com/SHU95/docker-env-go/domain"
+)
+
+type UserInteractor struct {
+	UserRepository UserRepository
+}
+
+func (interactor *UserInteractor) UserById(id int) (user domain.User, err error) {
+	user, err = interactor.UserRepository.FindByID(id)
+	return
+}
+
+func (interactor *UserInteractor) Add(createUser domain.User) (user domain.User, err error) {
+	user, err = interactor.UserRepository.Store(createUser)
+	return
+}
+
+func (interactor *UserInteractor) Update(updateUser domain.User) (user domain.User, err error) {
+	user, err = interactor.UserRespository.Update(updateUser)
+	return
+}
