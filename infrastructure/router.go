acbf2b45c370c6344f02df6fddef68bbfe713869
SHU <44566893+SHU95@users.noreply.github.com>
2021-02-25 16:41:22 +0900

    commit

---
+++ infrastructure/router.go
@@ -0,0 +1,26 @@
+package infrastructure
+
+import (
+	"github.com/SHU95/docker-env-go/interfaces/controllers"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+func Init() {
+	e := echo.New()
+
+	userController := controllers.NewUserController(NewMySqlDb())
+
+	//ログを出力
+	e.Use(middleware.Logger())
+
+	e.Use(middleware.Recover())
+
+	e.GET("/users/:id", func(context echo.Context) error { return userController.GetUser(context) })
+
+	e.POST("/users", func(context echo.Context) error { return userController.CreateUser(context) })
+
+	e.PUT("/users/:id", func(context echo.Context) error { return userController.UpdateUser(context) })
+
+	e.Logger.Fatal(e.Start(":8080"))
+}
---
+++ server.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/SHU95/docker-env-go/infrastructure"
+
+func main() {
+	infrastructure.Init()
+}
