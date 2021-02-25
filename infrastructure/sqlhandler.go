fbf7e6ce73697aa26f40a852a7d463a3a7bf4515
SHU <44566893+SHU95@users.noreply.github.com>
2021-02-25 13:04:52 +0900

    DBハンドラ作成
    DBの接続確認

---
+++ infrastructure/sqlhandler.go
@@ -0,0 +1,78 @@
+package infrastructure
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+type SqlHandler struct {
+	Conn *gorm.DB
+}
+
+func NewMySqlDb() database.SqlHandler {
+
+	// DBのコネクション情報
+	DBMS := "mysql"
+	USER := "root"
+	PASS := "####"
+	PROTOCOL := "tcp(##.###.##.###:3306)"
+	DBNAME := "##"
+
+	CONNECT := USER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME
+
+	conn, err := open(CONNECT, 30)
+
+	if err != nil {
+		panic(err)
+	}
+
+	//接続確認
+	err = conn.DB
+	if err != nil {
+		panic(err)
+	}
+
+	//log 出力
+	conn.LogMode(true)
+
+	//SQL文に`ENGINE=InnoDB`を付与
+	conn.Set("gorm:table_options", "ENGIN=InnoDB")
+
+	sqlHandler := new(SqlHandler)
+	sqlHandler.Conn = conn
+
+	return sqlHandler
+}
+
+//DBコンテナを起動確認→apiサーバコンテナを起動
+
+func open(path string, count unit) (*gorm.DB, error) {
+	db, err := gorm.Open("mysql", path)
+	if err != nil {
+		if count == 0 {
+			return nil, fmt.Errorf("Retry")
+		}
+		time.Sleep(time.Second)
+		count--
+		return open(path, count)
+	}
+	return db, nil
+}
+
+//検索
+func (handler *SqlHandler) Find(out interface{}, where ...interface{}) *gorm.DB {
+	return handler.Conn.Find(out, where...)
+}
+
+//作成
+func (handler *SqlHandler) Create(value interface{}) *gorm.DB {
+	return handler.Conn.Create(value)
+}
+
+//更新
+func (handler *SqlHandler) Save(value interface{}) *gorm.DB {
+	return handler.Conn.Save(value)
+}
