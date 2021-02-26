f32d725dfcfc585130c1e1336253f45089f7cbf3
SHU <44566893+SHU95@users.noreply.github.com>
2021-02-26 23:03:30 +0900

    commit
    jwt周りで実装方法に難あり

--- infrastructure/auth.go
+++ infrastructure/auth.go
@@ -1,1 +1,47 @@
 package infrastructure
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/labstack/echo"
+)
+
+func Login() echo.HandlerFunc {
+	return func(context echo.Context) error {
+		username := context.FormValue("username")
+		password := context.FormValue("password")
+
+		if username == "test" && password == "test" {
+			token := jwt.New(jwt.SigningMethodHS256)
+
+			claims := token.Claims.(jwt.MapClaims)
+			claims["name"] = "test" //username
+			claims["admin"] = true
+			claims["iat"] = time.Now().Unix()                     //発行日時
+			claims["exp"] = time.Now().Add(time.Hour * 72).Unix() //発行してからの有効期限
+
+			t, err := token.SignedString([]byte("secret"))
+			if err != nil {
+				return err
+			}
+			return context.JSON(http.StatusOK, map[string]string{
+				"token": t,
+			})
+		}
+
+		return echo.ErrUnauthorized
+
+	}
+}
+
+func Restricted() echo.HandlerFunc {
+	return func(context echo.Context) error {
+		user := context.Get("user").(*jwt.Token)
+		claims := user.Claims.(jwt.MapClaims)
+		name := claims["name"].(string)
+
+		return context.String(http.StatusOK, "Welcome"+name+"!")
+	}
+}
---
+++ interfaces/auth/jwt.go
@@ -0,0 +1,7 @@
+package auth
+
+//auth.goを変換する物を書けばOK
+
+type JWT struct {
+	Token string `json:"token"`
+}
