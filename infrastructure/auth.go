d557928d7377c91ae9f30d3ea478b58d0cc72883
SHU <44566893+SHU95@users.noreply.github.com>
2021-02-28 02:10:04 +0900

    commit
    エラーハンドリングとバリデーションの実装

--- infrastructure/auth.go
+++ infrastructure/auth.go
@@ -1,28 +1,75 @@
 package infrastructure
 
 import (
+	"fmt"
+	"text/template/parse"
 	"time"
 
 	"github.com/SHU95/docker-env-go/domain"
 	"github.com/dgrijalva/jwt-go"
+	"golang.org/x/tools/go/analysis/passes/nilfunc"
 )
 
 const (
 	secret  = "tukareteru"
-	id      = "id"
-	iatTime = "iat"
+	idKey  = "id"
+	iatKey = "iat"
 	expTime = "exp"
 )
 
-func createToken(user domain.User) (string, error) {
+//token 発行
+func CreateToken(user domain.User) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		id:      user.ID,
-		iatTime: user.CreatedAt.Unix(),
+		idKey:      user.ID,
+		iatKey: user.CreatedAt.Unix(),
 		expTime: user.CreatedAt.Add(time.Hour * 1000).Unix(),
 	})
 
 	return token.SignedString([]byte(secret))
 }
+
+//バリデーションチェック
+
+func VerifyToken(tokenStr string)(domain.User error){
+
+	parseToken, err := jwt.Parse(tokenStr, func(token *jwt.Token)(interface{}, error){
+
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			err := fmt.Errorf("unexpected signing method")
+			return nil ,err
+		}
+		return []byte(secret), nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	claims, ok := token.Claims.(jwt.Claims)
+
+	if !ok {
+		return nil fmt.Errorf("claims ヾ(｡>﹏<｡)ﾉ")
+	}
+	return domain.User, nil
+
+	id, ok := claims[idKey].(float64)
+	if !ok {
+		return nil fmt.Errorf("id ヾ(｡>﹏<｡)ﾉ")
+	}
+
+	iatTime, ok := claims[iatKey].(float64)
+	if !ok{
+		return nil fmt.Errorf("iat ヾ(｡>﹏<｡)ﾉ")
+	}
+	return (domain.User{
+		ID: uint(id),
+		CreatedAt: time.Unix(int64(iatTime), 0),
+	}, nil);
+}
+
+
+
+
+
+
 
 /*
 func Login() echo.HandlerFunc {
