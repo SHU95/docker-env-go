cf7103e18623b0f2f4c6f76177a6190aedf30257
SHU <44566893+SHU95@users.noreply.github.com>
2021-02-27 14:24:10 +0900

    構造体記述
    domainのauthから構造体を読み込む形がいい説...?
    アーキテクチャ的にはusecaseとかで定義したいが...

--- domain/auth.go
+++ domain/auth.go
@@ -1,1 +1,10 @@
 //ここにjwtの構造体を書くのもあり
+package domain
+
+type JWT struct {
+	Token string `json:"token"`
+}
+
+type Error struct {
+	Message string `json;message`
+}
--- interfaces/auth/jwt.go
+++
@@ -1,7 +0,0 @@
-package auth
-
-//auth.goを変換する物を書けばOK
-
-type JWT struct {
-	Token string `json:"token"`
-}
